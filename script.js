8e63e267bd13f90e0aa3b17de2739adaf56b426b
Vaishnavi226 <vaishnavitripathi226@gmail.com>
2026-03-08 18:33:13 +0530

    Weather app with updated js

---
+++ script.js
@@ -0,0 +1,61 @@
+const apiKey = "8f5deb3a69e94797849103659260803";
+const apiUrl = "https://api.weatherapi.com/v1/current.json?aqi=no&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector("#search-btn");
+
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&key=${apiKey}`);
+  const data = await response.json();
+
+  console.log(data);
+
+  document.querySelector(".city").innerHTML = data.location.name;
+
+  document.querySelector(".temp").innerHTML =
+    Math.round(data.current.temp_c) + "°C";
+
+  document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+
+  document.querySelector(".wind").innerHTML =
+    Math.round(data.current.wind_kph) + " km/h";
+
+  const weatherCondition = data.current.condition.text;
+
+  if (weatherCondition === "Clear") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\clear.png";
+  } else if (weatherCondition === "Sunny") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\clear.png";
+  } else if (weatherCondition === "Cloudy") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\clouds.png";
+  } else if (weatherCondition === "Partly cloudy") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\clouds.png";
+  } else if (weatherCondition === "Rain") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\rain.png";
+  } else if (weatherCondition === "Drizzle") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\drizzle.png";
+  } else if (weatherCondition === "Mist") {
+    weatherIcon.src =
+      "C:\Users\Siddhesh\Desktop\QSPIDER PROJECTS\Weather App\mist.png";
+  }
+
+  document.querySelector(".weather").style.display = "block";
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+});
